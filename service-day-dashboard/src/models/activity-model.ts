d7726078cb4858fab2bf4e7286aab341031015f1
codewhat7 <feiweng579@gmail.com>
2026-02-22 17:03:39 +0800

    activity model

---
+++ service-day-dashboard/src/models/activity-model.ts
@@ -0,0 +1,28 @@
+export interface Activity {
+  id:number;
+  title:string;
+  description:string;
+  date:string;
+  time:string;
+  location:string;
+  category:string;
+  totalSlots:string;
+  registeredSlots:string;
+  organization:string;
+  registeredDeadline:string;
+  difficulty:'Easy'|'Medium'|'Hard';
+}
+export interface Registration {
+  id: number;
+  employeeId: number;
+  activityId: number;
+  registrationDate: string;
+  status: 'Confirmed' | 'Waitlist' | 'Cancelled';
+}
+
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  department: string;
+}
