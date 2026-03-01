5c0d78c694d3776d8ee47712d1b22f77f77e39d0
codewhat7 <feiweng579@gmail.com>
2026-03-02 00:22:48 +0800

    check role and navigate to dashboard according to role

---
+++ service-day-dashboard/src/app/guards/staff.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { Router, CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const staffGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.isAdmin()) {
+    return true;
+  }
+  return router.parseUrl('/admin');
+};
