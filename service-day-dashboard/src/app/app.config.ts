dbaefbe794bee5735a80798d08e69533c84f948e
codewhat7 <feiweng579@gmail.com>
2026-03-02 15:46:29 +0800

    remove procideBrowserGlobalError replace by CoreModule

--- service-day-dashboard/src/app/app.config.ts
+++ service-day-dashboard/src/app/app.config.ts
@@ -1,14 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {provideHttpClient, withFetch} from '@angular/common/http';
+import {CoreModule} from './core/core.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
+    importProvidersFrom(CoreModule)
   ]
 };
