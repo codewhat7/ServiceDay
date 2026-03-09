ffd9ac659a662b8d3fa8c5dc894d884baec1e3c0
codewhat7 <feiweng579@gmail.com>
2026-03-09 20:48:44 +0800

    improve screen ui and remove unuse function

--- service-day-dashboard/src/app/admin/admin.module.ts
+++ service-day-dashboard/src/app/admin/admin.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { AdminDashboardComponent } from './admin-dashboard.component';
-import { AdminCreateComponent } from './admin-create.component'; // <-- Import the new component
+import { AdminCreateComponent } from './admin-create.component';
 
 const adminRoutes: Routes = [
   { path: '', component: AdminDashboardComponent },
-  { path: 'create', component: AdminCreateComponent } // <-- Add the create route!
+  { path: 'create', component: AdminCreateComponent }
 ];
 
 @NgModule({
